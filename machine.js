d6eab87ab123340cef2891dc908384e53075ed16
Salaatsarkar <salaatsarkar0501@gmail.com>
2026-02-26 21:44:04 +0400

    done card section with toggle

---
+++ machine.js
@@ -0,0 +1,5 @@
+function findId(id)
+{
+    const containerId = document.getElementById(id);
+    return containerId; 
+}
--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-const tabActive = ["bg-blue-600" , "text-white"];
+const tabActive = ["bg-blue-600", "text-white"];
 const tabInactive = ["bg-transparent", "text-neutral/50"];
 
-function toggleTab(tab){
-    const tabs = ["all","interview","rejected"];
-    for(const button of tabs)
-    {
-        const tabName = document.getElementById("tab-"+ button);
-        if(button === tab)
-        {
+const allContainer = findId('all-container');
+const interviewContainer = findId('interview-container');
+const rejectedContainer = findId('rejected-container');
+
+function toggleTab(tab) {
+    const tabs = ["all", "interview", "rejected"];
+    for (const button of tabs) {
+        const tabName = document.getElementById("tab-" + button);
+        if (button === tab) {
             tabName.classList.remove(...tabInactive);
             tabName.classList.add(...tabActive);
         }
-        else{
+        else {
             tabName.classList.remove(...tabActive);
             tabName.classList.add(...tabInactive);
         }
     }
-    
+    //add hidden class in clickable section
+    const sections = [allContainer, interviewContainer, rejectedContainer];
+    for (const section of sections) {
+        section.classList.add('hidden');
+    }
+
+    //remove clikable sections hidden class
+    if (tab === 'all') {
+        allContainer.classList.remove('hidden');
+    }
+    else if(tab === 'interview')
+    {
+        interviewContainer.classList.remove('hidden');
+    }
+    else{
+        rejectedContainer.classList.remove('hidden');
+    }
+
 }
 
 let currentTab = "all";
 toggleTab(currentTab);
+
+//dashboard number update
+
+const totalNumber = findId('total-number');
+const interviewNumber = findId('interview-number');
+const rejectNumber = findId('reject-number');
+
+totalNumber.innerText = allContainer.children.length;
+interviewNumber.innerText = interviewContainer.children.length;
+rejectNumber.innerText = rejectedContainer.children.length;
+
+//card's 3 button work 
+
+
+
