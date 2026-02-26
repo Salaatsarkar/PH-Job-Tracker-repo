e62e3f2f2a2a694b8021151ebfb95a9cfdd39d3a
Salaatsarkar <salaatsarkar0501@gmail.com>
2026-02-27 00:53:55 +0400

    updated dashboard and available job section and no jobs hide

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const tabInactive = ["bg-transparent", "text-neutral/50"];
 const allContainer = findId('all-container');
 const interviewContainer = findId('interview-container');
 const rejectedContainer = findId('rejected-container');
+const emptyContainer = findId('empty-container');
+const jobsCount = findId('jobs-count');
 
 function toggleTab(tab) {
     const tabs = ["all", "interview", "rejected"];
+    currentTab = tab;
+
     for (const button of tabs) {
         const tabName = document.getElementById("tab-" + button);
         if (button === tab) {
@@ -24,17 +28,28 @@ function toggleTab(tab) {
         section.classList.add('hidden');
     }
 
+    // emptyContainer.classList.add("hidden");
+
     //remove clikable sections hidden class
     if (tab === 'all') {
         allContainer.classList.remove('hidden');
+        if (allContainer.children.length < 1) {
+            emptyContainer.classList.remove("hidden");
+        }
     }
     else if (tab === 'interview') {
         interviewContainer.classList.remove('hidden');
+        if (interviewContainer.children.length < 1) {
+            emptyContainer.classList.remove("hidden");
+        }
     }
     else {
         rejectedContainer.classList.remove('hidden');
+        if (rejectedContainer.children.length < 1) {
+            emptyContainer.classList.remove("hidden");
+        }
     }
-
+    changeDashboard();
 }
 
 let currentTab = "all";
@@ -58,24 +73,41 @@ document.getElementById('parent-container').addEventListener('click', function (
     if (clickEventElement.classList.contains("interview")) {
         changeStatus.innerText = "Interviewed";
         interviewContainer.appendChild(card);
-        changeDashboard();
     }
     if (clickEventElement.classList.contains("reject")) {
         changeStatus.innerText = "Rejected";
         rejectedContainer.appendChild(card);
-        changeDashboard();
     }
     if (clickEventElement.classList.contains("delete")) {
         parent.removeChild(card);
-        changeDashboard();
     }
+
+    changeDashboard();
 
 })
 
 function changeDashboard() {
-    totalNumber.innerText = allContainer.children.length;
-    interviewNumber.innerText = interviewContainer.children.length;
-    rejectNumber.innerText = rejectedContainer.children.length;
+    // totalNumber.innerText = allContainer.children.length;
+    // interviewNumber.innerText = interviewContainer.children.length;
+    // rejectNumber.innerText = rejectedContainer.children.length;
+
+    const countObject = {
+        all: allContainer.children.length,
+        interview: interviewContainer.children.length,
+        rejected: rejectedContainer.children.length,
+    };
+
+    totalNumber.innerText = countObject.all;
+    interviewNumber.innerText = countObject.interview;
+    rejectNumber.innerText = countObject.rejected;
+
+    jobsCount.innerText = countObject[currentTab];
+    if(countObject[currentTab] < 1){
+        emptyContainer.classList.remove("hidden");
+    }
+    else{
+        emptyContainer.classList.add("hidden");
+    }
 }
 
 changeDashboard();
